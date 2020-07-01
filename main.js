363779454b56f14feae094419637004349a304a5
Mahir Juma <mahirdateflondon@gmail.com>
2020-07-01 04:20:51 +0300

    added some datasets & labels

--- main.js
+++ main.js
@@ -3,4 +3,14 @@ let myChart = document.getElementById("myChart").getContext("2d");
 let massChart = new Chart(myChart, {
   // bar,horizontal,pie
   type: "bar",
+  data: {
+    labels: ["Dar es Salaam", "Mwanza", "Arusha", "Morogoro", "Tanga"],
+    datasets: [
+      {
+        label: "Tanzania Population",
+        data: [2698652, 436801, 341136, 250902, 224876],
+      },
+    ],
+  },
+  options: {},
 });
