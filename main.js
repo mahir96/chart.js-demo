ca353c370194aa250ebff4becb1c54907b8af272
Mahir Juma <mahirdateflondon@gmail.com>
2020-07-01 04:34:58 +0300

    added rgba  colors in charts

--- main.js
+++ main.js
@@ -9,6 +9,14 @@ let massChart = new Chart(myChart, {
       {
         label: "Tanzania Population",
         data: [2698652, 436801, 341136, 250902, 224876],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
       },
     ],
   },
